b394094b0ad7799c23011ac6e9c470a7bdd78aad
haruki <soccer_haruki15@gmail.com>
2022-07-14 17:15:18 -0700

    fixed test error caused by not importing modules

--- instawork_homework/member/views.py
+++ instawork_homework/member/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
